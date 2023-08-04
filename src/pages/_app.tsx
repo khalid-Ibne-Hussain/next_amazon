bb5b02010c082c05051e63404f26d6e2aedae9de
khalid-Ibne-Hussain <khalidtuhinbd@gmail.com>
2023-08-04 20:20:41 +0600

    header,bottom header, footer done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
+import RootLayout from '@/components/RootLayout'
 import '@/styles/globals.css'
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='font-bodyFont bg-gray-300'>
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </div>
+  )
 }
---
+++ src/pages/cart.tsx
@@ -0,0 +1,16 @@
+import Footer from '@/components/Footer';
+import BottomHeader from '@/components/header/BottomHeader';
+import Header from '@/components/header/Header';
+import React from 'react';
+
+
+
+const CartPage = () => {
+    return (
+        <div>
+            cart
+        </div>
+    );
+};
+
+export default CartPage;
